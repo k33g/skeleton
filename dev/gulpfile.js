24ab8c0577976181d1b9a2928940e6a5cbbc3821
k33g <ph.charriere@gmail.com>
2014-10-19 22:44:04 +0200

    First commit
    add traceur 0.0.66

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -7,6 +7,7 @@ gulp.task('dist', function() {
   fs.unlink('../*.js', function (err) {
     if (err) { console.log(err.message); };
     gulp.src('public/js/skeleton/*.js').pipe(gulp.dest('../'));
+    gulp.src('node_modules/traceur/bin/traceur.js').pipe(gulp.dest('../'));
   });
 
 });
